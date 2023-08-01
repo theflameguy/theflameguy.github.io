9913d9f0d8a8cb0ffdee97a986dcf073d409f27d
Abhishek Singh <abhishek.s@orchids.edu.in>
2023-08-01 15:18:45 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,22 @@
+
+import Intro from "./components/intro/Intro";
+import About from "./components/about/About";
+import ProductList from "./components/productList/ProductList";
+import Contact from "./components/contact/Contact";
+
+
+
+function App() {
+
+  return (
+    <div>
+      <Intro />
+      <About />
+      <ProductList />
+      <Contact />
+      
+    </div>
+  );
+}
+
+export default App;
---
+++ src/data.js
@@ -0,0 +1,32 @@
+export const products = [
+  {
+    id: 1,
+    img: "https://wallpapers.com/images/hd/maple-leaves-mobile-v55ozuzaohuq89hh.jpg",
+    link: "http://lama.dev",
+  },
+  {
+    id: 2,
+    img: "https://cdn.wallpapersafari.com/16/69/rcj6Cz.jpg",
+    link: "http://lama.dev",
+  },
+  {
+    id: 3,
+    img: "",
+    link: "http://lama.dev",
+  },
+  {
+    id: 4,
+    img: "",
+    link: "http://lama.dev",
+  },
+  {
+    id: 5,
+    img: "",
+    link: "http://lama.dev",
+  },
+  {
+    id: 6,
+    img: "",
+    link: "http://lama.dev",
+  },
+];
